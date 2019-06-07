32c58bf2a4922111c00e533d79f5905d088657a5
Tom Jardine-McNamara <tom@itsravenous.com>
2019-06-07 06:47:06 +0100

    Add base and form-specific styles
    In a more complex app I would likely make more use of utility classes
    for margins, etc.

--- src/PickupSearch.js
+++ src/PickupSearch.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './PickupSearch.css';
 
 export const PickupSearch = () => (
-  <form>
-    <h2>Where are you going?</h2>
-    <label htmlFor="pickup">Pick-up Location</label>
-    <input
-      id="pickup"
-      type="text"
-      placeholder="city, airport, station, region, district…"
-    />
+  <form className="c-pickup-search">
+    <h2 className="c-heading">Where are you going?</h2>
+    <div className="c-form-field">
+      <label htmlFor="pickup">Pick-up Location</label>
+      <input
+        id="pickup"
+        type="text"
+        placeholder="city, airport, station, region, district…"
+      />
+    </div>
   </form>
 );
